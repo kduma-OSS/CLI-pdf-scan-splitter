75f4bef1a5fe21c0ea20c47e851d5ccbf01c8ef0
Krystian Duma <git@krystian.duma.sh>
2022-11-29 23:29:52 +0100

    Use global temp dir when packaged in phar

--- app/Actions/Tools/TemporaryDirectoryCreatorAction.php
+++ app/Actions/Tools/TemporaryDirectoryCreatorAction.php
@@ -2,13 +2,19 @@
 
 namespace App\Actions\Tools;
 
+use Phar;
 use Spatie\TemporaryDirectory\TemporaryDirectory;
 
 class TemporaryDirectoryCreatorAction
 {
+    protected function isPhar(): bool
+    {
+        return strlen(Phar::running()) > 0;
+    }
+
     public function get(): TemporaryDirectory
     {
-        return new TemporaryDirectory(base_path('tmp'));
+        return new TemporaryDirectory($this->isPhar() ? sys_get_temp_dir() : base_path('tmp'));
     }
 
     public function create(): TemporaryDirectory
