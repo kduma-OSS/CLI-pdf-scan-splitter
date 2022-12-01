59766b664124121346a39a98430767b319c4dc18
Krystian Duma <git@krystian.duma.sh>
2022-12-01 23:49:55 +0100

    Limit scanned barcodes

--- bin/barcode-scanner/run.sh
+++ bin/barcode-scanner/run.sh
@@ -2,4 +2,4 @@
 
 /usr/bin/pdftoppm -png -r $1 -singlefile -cropbox '/data/input.pdf' '/data/image' || exit 1
 
-/usr/bin/zbarimg -S*.enable  '/data/image.png' > '/data/output.txt' || exit 5
+/usr/bin/zbarimg --set '*.disable' --set '*.no-position' --set '*.x-density=2' --set '*.y-density=2' --set 'code128.enable' --set 'qrcode.enable'  '/data/image.png' > '/data/output.txt' || exit 5
